935d66236e35866e02cebbf3338b540be017a9a8
EhabHamid <ehabhamidalx@gmail.com>
2023-09-03 21:58:31 -0700

    {New commit drop here}

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -1,25 +1,64 @@
 #include "main.h"
+#include <stdio.h>
 #include <stdlib.h>
+
 /**
-* ch_free_grid - frees a 2 dimensional array.
-* @grid: multidimensional array of char.
-* @height: height of the array.
-*
-* Return: no return
-*/
-void ch_free_grid(char **grid, unsigned int height)
+ * strtow - concatenates all the arguments of your program
+ *@str: string
+ *@av: arguments
+ * Return: a pointer to a new string
+ */
+char **strtow(char *str)
 {
-if (grid != NULL && height != 0)
-{
-for (; height > 0; height--)
-free(grid[height]);
-free(grid[height]);
-free(grid);
+	int i, w, j, k, count, m, wordf;
+	char **p;
+	char *x;
+
+	w = 0;
+	j = 0;
+	i = 0;
+	count = 0;
+	if (*str == '\0' || str == NULL)
+		return (NULL);
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		if (str[i] == ' ' && (str[i + 1] != ' ' || str[i + 1] == '\0'))
+			w++;
+	}
+	p = (char **)malloc((w + 1) * sizeof(char *));
+	if (p == NULL)
+		return (NULL);
+	for (wordf = 0; str[wordf] && j <= w; wordf++)
+	{
+		count = 0;
+		if (str[wordf] != ' ')
+		{
+			for (i = wordf ; str[i] != '\0'; i++)
+			{
+				if (str[i] == ' ')
+					break;
+				count++;
+			}
+			*(p + j) = (char *)malloc((count + 1) * sizeof(char));
+			if (*(p + j) == NULL)
+			{
+				for (k = 0; k <= j; k++)
+				{
+					x = p[k];
+					free(x);
+				}
+				free(p);
+				return (NULL);
+			}
+			for (m = 0; wordf < i; wordf++)
+			{
+				p[j][m] = str[wordf];
+				m++;
+			}
+			p[j][m] = '\0';
+			j++;
+		}
+	}
+	p[j] = NULL;
+	return (p);
 }
-}
-/**
-* strtow - splits a string into words.
-* @str: string.
-*
-* Return: pointer of an array of integers
-*/
